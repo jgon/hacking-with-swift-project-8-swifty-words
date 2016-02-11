a9d2214e2ac2ba28a84dc983e9578ac449da3ed4
JackG <jacques.gonzalez@me.com>
2016-02-11 14:24:55 +0100

    Laid out buttons and labels.

--- Project8/ViewController.swift
+++ Project8/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cluesLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var currentAnswer: UITextField!
+    @IBOutlet weak var scoreLabel: UILabel!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func submitTapped(sender: AnyObject) {
+    }
 
+    @IBAction func clearTapped(sender: AnyObject) {
+    }
 }
 
